102cb25f8c944537d1a57a793c96723311f7c7ab
Keane Quibilan <arv.r2vq@gmail.com>
2022-11-22 22:49:04 -0500

    Stylize New Room

--- src/NewRoom/NewRoom.js
+++ src/NewRoom/NewRoom.js
@@ -1,35 +1,44 @@
 import { addDoc, collection } from "firebase/firestore";
 import { createRef } from "react";
+import "./NewRoom.css";
 
 function NewRoom({ db, setNewRoom, userId, setRoomId }) {
 
     const inputRef = createRef();
 
     return (
-        <div>
+        <div
+            className="NewRoom-container"
+        >
+            Create a new room:
             <input
                 className="NewRoom-input"
+                placeholder="Room Name"
                 ref={inputRef}
                 type="text"
             ></input>
-            <button
-                onClick={() => {
-                    setNewRoom(false);
-                }}
-            >
-                Cancel
-            </button>
-            <button
-                onClick={async () => {
-                    await addDoc(collection(db, "rooms"), {
-                        roomName: inputRef.current.value,
-                        roomId: userId
-                    });
-                    setRoomId(userId);
-                }}
-            >
-                Save
-            </button>
+            <div class="NewRoom-buttons">
+                <button
+                    className="NewRoom-button"
+                    onClick={() => {
+                        setNewRoom(false);
+                    }}
+                >
+                    Cancel
+                </button>
+                <button
+                    className="NewRoom-button"
+                    onClick={async () => {
+                        await addDoc(collection(db, "rooms"), {
+                            roomName: inputRef.current.value,
+                            roomId: userId
+                        });
+                        setRoomId(userId);
+                    }}
+                >
+                    Save
+                </button>
+            </div>
         </div>
     );
 }
